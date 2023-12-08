6910d325ad8634e3308fe7311c76178bf4e55748
scaminom <jcamino7758@uta.edu.ec>
2023-12-08 16:57:42 -0500

    Implementing calculations for items and order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :product
-  belongs_to :order
+  belongs_to :order, foreign_key: 'order_number', primary_key: 'order_number'
+
+  before_save :set_unit_price
+  before_save :calculate_subtotal
+  after_save  :update_order_total
+
+  private
+
+  def update_order_total
+    return unless order.present?
+
+    order.calculate_total
+  end
+
+  def calculate_subtotal
+    self.subtotal = quantity * unit_price
+  end
+
+  def set_unit_price
+    self.unit_price = product.price
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,24 @@
 class Order < ApplicationRecord
-  belongs_to :table
-  belongs_to :waiter, class_name: 'User', foreign_key: 'waiter_id'
+  belongs_to  :table
+  belongs_to  :waiter, class_name: 'User', foreign_key: 'waiter_id'
+  has_many    :items, foreign_key: 'order_number', primary_key: 'order_number'
+  before_save :set_date_time_now
 
   enum status: {
     'in_process': 1,
     'ready': 3,
     'completed': 4
   }
+
+  def calculate_total
+    new_total = items.sum(&:subtotal)
+
+    return unless new_total != total
+
+    update_column(:total, new_total)
+  end
+
+  def set_date_time_now
+    self.date = Time.now
+  end
 end
