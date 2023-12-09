c2ef002e648293f74192ef1d4f5c4dfe58c939cf
Agustin Camino <agustin.camino@gmail.com>
2023-12-09 17:18:53 -0500

    Some updates for your work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # rescue_from ActionController::RoutingError, with: :not_found_method
+
+  private
+
   def not_found_method
     route_info = "No route matches [#{request.method}] \"#{request.original_fullpath}\""
     render json: { error: route_info }.to_json, status: :not_found, layout: false
+  end
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,15 @@
 class Client < ApplicationRecord
+  WHITELISTED_ATTRIBUTES = [
+    :id,
+    :first_name,
+    :last_name,
+    :address,
+    :email,
+    :phone,
+    :date,
+    :id_type
+  ].frozen
+
   enum id_type: {
     'RUC': 1,
     'CEDULA': 2
