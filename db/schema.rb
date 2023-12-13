11246317ad393a9ce5a372b0e469b42d5417f381
scaminom <jcamino7758@uta.edu.ec>
2023-12-13 18:07:49 -0500

    Fixed sign_out problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_213840) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_13_225748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_213840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
