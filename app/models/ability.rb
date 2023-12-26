7f4c500adfc4a62cd938ac764ae115a0c202516d
scaminom <jcamino7758@uta.edu.ec>
2023-12-26 11:29:01 -0500

    Give permision in waiters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
 
   def initialize(user)
     can :manage, :all if user.admin?
-    can %i[read create], Order if user.waiter?
+    can %i[read create dispatch_item], Order if user.waiter?
     # can %i[read], Event if user.waiter?
     can %i[read update in_process dispatch_item], Order if user.cook?
   end
