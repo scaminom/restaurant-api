9c83aae4f08d26c19dc77f9b28a837ebbdd28c67
scaminom <jcamino7758@uta.edu.ec>
2023-12-25 23:02:16 -0500

    Fix order creator

--- app/services/order_processing_service.rb
+++ app/services/order_processing_service.rb
@@ -4,7 +4,7 @@ class OrderProcessingService
   end
 
   def process_order_on_create
-    order_publisher = Services::Post::CreateOrderWhisper.new
+    order_publisher = Post::CreateOrderWhisper.new
     create_event_listener = Listeners::CreateEventListener.new
     order_publisher.publish_order_creation(@order)
     order_publisher.subscribe(create_event_listener.order_created(@order))
@@ -12,7 +12,7 @@ class OrderProcessingService
   end
 
   def process_order_on_update
-    order_publisher = Services::Post::CreateOrderWhisper.new
+    order_publisher = Post::CreateOrderWhisper.new
     update_event_listener = Listeners::UpdateEventListener.new
     order_publisher.publish_order_creation(@order)
     order_publisher.subscribe(update_event_listener.order_modified(@order))
@@ -20,7 +20,7 @@ class OrderProcessingService
   end
 
   def process_order_on_ready
-    order_publisher = Services::Post::CreateOrderWhisper.new
+    order_publisher = Post::CreateOrderWhisper.new
     update_event_listener = Listeners::UpdateEventListener.new
     order_publisher.publish_order_creation(@order)
     order_publisher.subscribe(update_event_listener.order_ready(@order))
@@ -28,7 +28,7 @@ class OrderProcessingService
   end
 
   def process_order_in_process
-    order_publisher = Services::Post::CreateOrderWhisper.new
+    order_publisher = Post::CreateOrderWhisper.new
     update_event_listener = Listeners::UpdateEventListener.new
     order_publisher.publish_order_creation(@order)
     order_publisher.subscribe(update_event_listener.order_in_process(@order))
