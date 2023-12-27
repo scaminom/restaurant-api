e2065ffd8557e79319bb35a0e5d3600476017fbc
scaminom <jcamino7758@uta.edu.ec>
2023-12-27 17:41:20 -0500

    Fix abilitites

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
   def initialize(user)
     can :manage, :all if user.admin?
     can %i[read update create dispatch_item dispatch_order distroy], Order if user.waiter?
-    can %i[read update], Table if user.waiter?
+    can %i[read occupy release], Table if user.waiter?
     can :manage, Item if user.waiter? || user.cook?
-    can :manage, Product if user.waiter? || user.cook?
+    can :read, Product if user.waiter? || user.cook?
     can %i[read in_process dispatch_item], Order if user.cook?
   end
 end
