fa60fb93bbb9ec7854de15a3045142f9357dbd2c
scaminom <jcamino7758@uta.edu.ec>
2023-12-27 17:32:13 -0500

    Fix abilitites

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,8 @@ class Ability
     can :manage, :all if user.admin?
     can %i[read update create dispatch_item dispatch_order distroy], Order if user.waiter?
     can %i[read update], Table if user.waiter?
-    can :manage, Item if user.waiter?
+    can :manage, Item if user.waiter? || user.cook?
+    can :manage, Product if user.waiter? || user.cook?
     can %i[read in_process dispatch_item], Order if user.cook?
   end
 end
