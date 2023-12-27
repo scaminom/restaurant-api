aa62e98ee9dcfc0e2eb04a629f38287c368284c6
scaminom <jcamino7758@uta.edu.ec>
2023-12-27 15:24:10 -0500

    Modify cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins %w[https://restaurant-uta.netlify.app http://localhost:5173]
 
     resource '*',
              headers: :any,
