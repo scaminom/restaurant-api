a58a77b265668fef5932166b2b3ba16e94c128e6
scaminom <jcamino7758@uta.edu.ec>
2023-12-26 23:48:07 -0500

    Allow update waiter user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,8 @@ class Ability
 
   def initialize(user)
     can :manage, :all if user.admin?
-    can %i[read create dispatch_item dispatch_order], Order if user.waiter?
+    can %i[read update create dispatch_item dispatch_order], Order if user.waiter?
     # can %i[read], Event if user.waiter?
-    can %i[read update in_process dispatch_item], Order if user.cook?
+    can %i[read in_process dispatch_item], Order if user.cook?
   end
 end
