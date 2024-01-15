7ccfa0fefacccda5312e2e0eeaa171af526292dc
scaminom <jcamino7758@uta.edu.ec>
2024-01-14 22:59:15 -0500

    Modify can can for specific allowed admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.admin?
+    can :manage, User if user.admin?
+    can %i[read create update], Client if user.admin?
+    can %i[read create], Invoice if user.admin?
     can %i[read update create dispatch_item dispatch_order distroy], Order if user.waiter?
     can %i[read occupy release], Table if user.waiter? || user.cashier?
     can %i[read update], Order if user.cashier?
