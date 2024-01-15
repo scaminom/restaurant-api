ee4b94b5e6017fbdf740635d6a11c1562e6e3353
scaminom <jcamino7758@uta.edu.ec>
2024-01-14 22:30:12 -0500

    Fix rspec invoice controller

--- spec/controllers/invoice_controller_spec.rb
+++ spec/controllers/invoice_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe InvoicesController, type: :controller do
   end
 
   describe 'POST #create' do
-    let(:order) { create(:order, :with_waiter) }
+    let(:order) { create(:order, :with_waiter, status: 'dispatched') }
     let(:client) { create(:client, id: '0504427758') }
     let(:invoice) { create(:invoice) }
     let(:valid_params) do
@@ -68,52 +68,6 @@ RSpec.describe InvoicesController, type: :controller do
         response_body = JSON.parse(response.body)
         expect(response_body).to be_present
       end
-    end
-  end
-
-  describe 'PUT #update' do
-    let(:invoice) { create(:invoice) }
-    let(:valid_params) { { id: invoice.id, invoice: { payment_method: 'online' } } }
-    let(:invalid_params) { { id: invoice.id, invoice: { payment_method: 'test' } } }
-
-    context 'with valid parameters' do
-      it 'updates the invoice' do
-        put :update, params: valid_params
-        expect(invoice.reload.payment_method).to eq('online')
-      end
-
-      it 'returns a successful response' do
-        put :update, params: valid_params
-        expect(response).to have_http_status(:success)
-      end
-    end
-
-    context 'with invalid parameters' do
-      it 'does not update the invoice' do
-        put :update, params: invalid_params
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it 'returns an error message' do
-        put :update, params: invalid_params
-        response_body = JSON.parse(response.body)
-        expect(response_body).to be_present
-      end
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    let!(:invoice) { create(:invoice) }
-
-    it 'destroys the invoice' do
-      expect do
-        delete :destroy, params: { id: invoice.id }
-      end.to change(Invoice, :count).by(-1)
-    end
-
-    it 'returns a successful response' do
-      delete :destroy, params: { id: invoice.id }
-      expect(response).to have_http_status(:success)
     end
   end
 end
